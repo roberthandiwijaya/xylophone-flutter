9cfc12fcd06546bd707d17c3c448713b139cfc7c
Robert Handi Wijaya <roberthandiwijaya@gmail.com>
2020-09-07 00:21:17 +0700

    Added Color Sound.

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,63 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void PlaySound(int SoundNumber){
+    final player = AudioCache();
+    player.play('note$SoundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play(('note1.wav'));
-              },
-              child: Text('Text Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  PlaySound(1);
+                },
+                color: Colors.red
+              ),
+              FlatButton(
+                  onPressed: () {
+                    PlaySound(2);
+                  },
+                  color: Colors.orange
+              ),
+              FlatButton(
+                  onPressed: () {
+                    PlaySound(3);
+                  },
+                  color: Colors.yellow
+              ),
+              FlatButton(
+                  onPressed: () {
+                    PlaySound(4);
+                  },
+                  color: Colors.green
+              ),
+              FlatButton(
+                  onPressed: () {
+                    PlaySound(5);
+                  },
+                  color: Colors.teal
+              ),
+              FlatButton(
+                  onPressed: () {
+                    PlaySound(6);
+                  },
+                  color: Colors.blueAccent
+              ),
+              FlatButton(
+                  onPressed: () {
+                    PlaySound(7);
+                  },
+                  color: Colors.purple
+              ),
+            ],
           ),
         ),
       ),
