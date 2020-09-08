0a41b4f25b67ca03dcf7bdaaa65cd1fb999c7a4b
Robert Handi Wijaya <roberthandiwijaya@gmail.com>
2020-09-08 07:32:49 +0700

    Updating Ui Of the Apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-
-  void PlaySound(int SoundNumber){
+  void PlaySound(int SoundNumber) {
     final player = AudioCache();
     player.play('note$SoundNumber.wav');
   }
@@ -16,49 +16,57 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  PlaySound(1);
-                },
-                color: Colors.red
+              Container(
+                padding: EdgeInsets.symmetric(vertical:100),
+                child: FlatButton(
+                    onPressed: () {
+                      PlaySound(1);
+                    },
+                    color: Colors.red),
               ),
-              FlatButton(
-                  onPressed: () {
-                    PlaySound(2);
-                  },
-                  color: Colors.orange
+              Container(
+                child: FlatButton(
+                    onPressed: () {
+                      PlaySound(2);
+                    },
+                    color: Colors.orange),
               ),
-              FlatButton(
-                  onPressed: () {
-                    PlaySound(3);
-                  },
-                  color: Colors.yellow
+              Container(
+                child: FlatButton(
+                    onPressed: () {
+                      PlaySound(3);
+                    },
+                    color: Colors.yellow),
               ),
-              FlatButton(
-                  onPressed: () {
-                    PlaySound(4);
-                  },
-                  color: Colors.green
+              Container(
+                child: FlatButton(
+                    onPressed: () {
+                      PlaySound(4);
+                    },
+                    color: Colors.green),
               ),
-              FlatButton(
-                  onPressed: () {
-                    PlaySound(5);
-                  },
-                  color: Colors.teal
+              Container(
+                child: FlatButton(
+                    onPressed: () {
+                      PlaySound(5);
+                    },
+                    color: Colors.teal),
               ),
-              FlatButton(
-                  onPressed: () {
-                    PlaySound(6);
-                  },
-                  color: Colors.blueAccent
+              Container(
+                child: FlatButton(
+                    onPressed: () {
+                      PlaySound(6);
+                    },
+                    color: Colors.blueAccent),
               ),
-              FlatButton(
-                  onPressed: () {
-                    PlaySound(7);
-                  },
-                  color: Colors.purple
+              Container(
+                child: FlatButton(
+                    onPressed: () {
+                      PlaySound(7);
+                    },
+                    color: Colors.purple),
               ),
             ],
           ),
